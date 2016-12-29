64a7ed2453c670626b8a70d78577d0d944fd1d73
Haojian Wu <hokein.wu@gmail.com>
2016-12-29 19:58:52 +0100

    Add play webm functionality.

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const {desktopCapturer, ipcRenderer} = require('electron')
+const {desktopCapturer, ipcRenderer, remote} = require('electron')
 const domify = require('domify')
 
 let localStream
@@ -13,12 +13,22 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#record-desktop').addEventListener('click', recordDesktop)
   document.querySelector('#record-camera').addEventListener('click', recordCamera)
   document.querySelector('#record-window').addEventListener('click', recordWindow)
+  document.querySelector('#play-video').addEventListener('click', playVideo)
   document.querySelector('#micro-audio').addEventListener('click', microAudioCheck)
   // document.querySelector('#system-audio').addEventListener('click', sysAudioCheck)
   document.querySelector('#record-stop').addEventListener('click', stopRecording)
   document.querySelector('#play-button').addEventListener('click', play)
   document.querySelector('#download-button').addEventListener('click', download)
 })
+
+const playVideo = () => {
+  remote.dialog.showOpenDialog({properties: ['openFile']}, (filename) => {
+    console.log(filename)
+    let video = document.querySelector('video')
+    video.muted = false
+    video.src = filename
+  })
+}
 
 const disableButtons = () => {
   document.querySelector('#record-desktop').disabled = true
