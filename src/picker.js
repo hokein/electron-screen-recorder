ed8dec669bebbe1e85595ae5f0aca2b0a4b15db2
jasonjarvan <jarvanzhao@qq.com>
2021-07-23 18:06:37 +0800

    update to electron 12.0.5, fix compatibility issue in picker

--- src/picker.js
+++ src/picker.js
@@ -1,4 +1,4 @@
-const {desktopCapturer, ipcRenderer} = require('electron')
+const { desktopCapturer, ipcRenderer } = require('electron')
 const domify = require('domify')
 
 document.onkeydown = function (evt) {
@@ -9,27 +9,26 @@ document.onkeydown = function (evt) {
   }
 }
 
-ipcRenderer.on('get-sources', (event, options) => {
-  desktopCapturer.getSources(options, (error, sources) => {
-    if (error) throw error
-    let sourcesList = document.querySelector('.capturer-list')
-    for (let source of sources) {
-      let thumb = source.thumbnail.toDataURL()
-      if (!thumb) continue
-      let title = source.name.slice(0, 20)
-      let item = `<li><a href="#"><img src="${thumb}"><span>${title}</span></a></li>`
-      sourcesList.appendChild(domify(item))
-    }
-    let links = sourcesList.querySelectorAll('a')
-    for (let i = 0; i < links.length; ++i) {
-      let closure = (i) => {
-        return (e) => {
-          e.preventDefault()
-          ipcRenderer.send('source-id-selected', sources[i].id)
-          sourcesList.innerHTML = ''
-        }
+ipcRenderer.on('get-sources', async (event, options) => {
+  const sources = await desktopCapturer.getSources(options);
+
+  let sourcesList = document.querySelector('.capturer-list')
+  for (let source of sources) {
+    let thumb = source.thumbnail.toDataURL()
+    if (!thumb) continue
+    let title = source.name.slice(0, 20)
+    let item = `<li><a href="#"><img src="${thumb}"><span>${title}</span></a></li>`
+    sourcesList.appendChild(domify(item))
+  }
+  let links = sourcesList.querySelectorAll('a')
+  for (let i = 0; i < links.length; ++i) {
+    let closure = (i) => {
+      return (e) => {
+        e.preventDefault()
+        ipcRenderer.send('source-id-selected', sources[i].id)
+        sourcesList.innerHTML = ''
       }
-      links[i].onclick = closure(i)
     }
-  })
+    links[i].onclick = closure(i)
+  }
 })
